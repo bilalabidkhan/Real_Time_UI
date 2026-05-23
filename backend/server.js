8556f71fc86705c677397612f2138ba9f005970e
bilalabidkhan <bilalabid6578@gmail.com>
2026-05-23 23:23:44 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("Backend running on port 3001");
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log("Backend running on port " + PORT);
 });
